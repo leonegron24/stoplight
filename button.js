fabdb0c5c3c60f7cd62296ef7bbfdf4d3546b58e
Leonardo Negron <leonegron24@gmail.com>
2024-09-15 20:58:42 -0600

    stoplight

---
+++ button.js
@@ -0,0 +1,11 @@
+document.getElementById('glowButtonRed').addEventListener('click', function() {
+    this.classList.toggle('glow');
+});
+
+document.getElementById('glowButtonYellow').addEventListener('click', function() {
+    this.classList.toggle('glow');
+});
+
+document.getElementById('glowButtonGreen').addEventListener('click', function() {
+    this.classList.toggle('glow');
+});
